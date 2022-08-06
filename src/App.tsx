56806d4f990a95d8965c2b623089b1df6f2e6fcc
mothepro <4e688d8adf9b8f68774900f9809778ccb7ef857a>
2022-08-05 23:38:43 -0500

    add dev tools

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useRef, useState } from 'react'
+import { KeyboardEvent, useRef, useState } from 'react'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import './App.css'
 import GithubStats from './GithubStats'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient
 
@@ -9,17 +10,26 @@ export default function App() {
   const [username, setUsername] = useState('')
   const usernameEl = useRef<HTMLInputElement>(null)
 
+  const handleSubmit = () => setUsername(usernameEl.current!.value.trim())
+  const handleUsernameKeyDown = ({ key }: KeyboardEvent<HTMLInputElement>) => key === 'Enter' && handleSubmit()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <label>
-          <input type="text" defaultValue={username} ref={usernameEl} placeholder="Github Username" />
-          <input type="submit" onClick={() => setUsername(usernameEl.current!.value.trim())} value="Go" />
-        </label>
-        <QueryClientProvider client={queryClient}>
-          <GithubStats username={username} />
-        </QueryClientProvider>
-      </header>
-    </div>
-  );
+    <>
+      <header>Find Dependency Skill</header>
+      <label>
+        <input
+          type='text'
+          defaultValue={username}
+          ref={usernameEl}
+          onKeyDown={handleUsernameKeyDown}
+          placeholder='Github Username'
+        />
+        <input type='submit' onClick={handleSubmit} value='Go' />
+      </label>
+      <QueryClientProvider client={queryClient}>
+        <GithubStats username={username} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  )
 }
